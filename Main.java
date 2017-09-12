c2218ba0f35d1caaeeed59b3d9fa8eba46ab2e5b
nikhil567 <31884343+nikhil567@users.noreply.github.com>
2017-09-12 17:20:09 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		 Scanner s = new Scanner(System.in);
+		 int n=s.nextInt();
+		StudentGroup s= new StudentGroup(n);
+		
+	}
+
+}
